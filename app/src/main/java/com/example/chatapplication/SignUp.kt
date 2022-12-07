b68ac0956d1cc793c41d5afc575cdbd7c34de490
Rohit George <rohitgeorge2000@gmail.com>
2022-12-07 09:33:38 +0530

    first commit

---
+++ app/src/main/java/com/example/chatapplication/SignUp.kt
@@ -0,0 +1,26 @@
+package com.example.chatapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class SignUp : AppCompatActivity() {
+
+    private lateinit var btnlog : Button
+    private lateinit var btnSignUp: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        btnlog = findViewById(R.id.btnlog)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnlog.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+    }
+}
