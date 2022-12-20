5d4488f9685a1922be9ba0ff05f5c5173c1d4df7
Rohit George <rohitgeorge2000@gmail.com>
2022-12-20 20:57:34 +0530

    third commit

--- app/src/main/java/com/example/chatapplication/MeetingMD.kt
+++ app/src/main/java/com/example/chatapplication/MeetingMD.kt
@@ -1,11 +1,60 @@
 package com.example.chatapplication
 
+import android.app.DatePickerDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MeetingMD : AppCompatActivity() {
+
+    lateinit var pickDateBtn: Button
+    lateinit var selectedDateTV: TextView
+    lateinit var st1: Button
+    lateinit var st2: Button
+    lateinit var st3: Button
+    lateinit var st4: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meeting_md)
+
+        st1 = findViewById(R.id.time_option1)
+        st2 = findViewById(R.id.time_option2)
+        st3 = findViewById(R.id.time_option3)
+        pickDateBtn = findViewById(R.id.idBtnPickDate)
+        selectedDateTV = findViewById(R.id.idTVSelectedDate)
+
+        st1.setOnClickListener {
+            st1.setBackgroundColor(Color.parseColor("#D2D2D2"))
+        }
+        st2.setOnClickListener {
+            st2.setBackgroundColor(Color.parseColor("#D2D2D2"))
+        }
+        st3.setOnClickListener {
+            st3.setBackgroundColor(Color.parseColor("#D2D2D2"))
+        }
+
+        pickDateBtn.setOnClickListener {
+            val c = Calendar.getInstance()
+
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(
+                this,
+                {
+                    view, year, monthOfYear, dayOfMonth ->
+                    selectedDateTV.text =
+                        (dayOfMonth.toString() + "-" + (monthOfYear+1) + "-" +year)
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
     }
 }
--- app/src/main/java/com/example/chatapplication/faq.kt
+++ app/src/main/java/com/example/chatapplication/faq.kt
@@ -1,11 +1,71 @@
 package com.example.chatapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class faq : AppCompatActivity() {
+
+//    private lateinit var edtEmail : EditText
+//    private lateinit var edtPassword : EditText
+//    private lateinit var btnMeeting : Button
+//    private lateinit var btnSignup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faq)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+// Set Home selected
+        bottomNavigationView.selectedItemId = R.id.home
+
+// Perform item selected listener
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.circle -> {
+                    startActivity(Intent(applicationContext, MeetingMD::class.java))
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.home -> return@OnNavigationItemSelectedListener true
+//                R.id.metro -> {
+//                    startActivity(Intent(applicationContext, MetroSceen::class.java))
+//                    overridePendingTransition(0, 0)
+//                    return@OnNavigationItemSelectedListener true
+//                }
+//                R.id.map -> {
+//                    startActivity(Intent(applicationContext, MapActivity::class.java))
+//                    overridePendingTransition(0, 0)
+//                    return@OnNavigationItemSelectedListener true
+//                }
+//                R.id.user -> {
+//                    startActivity(Intent(applicationContext, UserActivity::class.java))
+//                    overridePendingTransition(0, 0)
+//                    return@OnNavigationItemSelectedListener true
+//                }
+
+            }
+            false
+        })
+
+//        btnMeeting = findViewById(R.id.circle)
+////
+//        btnMeeting.setOnClickListener {
+//            val intent = Intent(this, MeetingMD::class.java)
+//            startActivity(intent)
+//        }
+
+
+//        btnLogin.setOnClickListener {
+//            val inten = Intent(this, faq::class.java)
+//            startActivity(inten)
+//        }
+
     }
 }
